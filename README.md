e741114e7572f6c16d106e3d3482f1d352bf2572
llnfrnk <lilianagomezmedina@gmail.com>
2019-03-26 21:16:14 +0100

    Ok

---
+++ README.md
@@ -0,0 +1,3 @@
+APP para llamar a una API y generar usuarios de forma aleatoria
+
+Utilizaremos JQuery
--- js/mi.js
+++ js/mi.js
@@ -1,15 +1,15 @@
 // 1 - variables
 const boton = $("#boton");
 const contenido = $("#contenido");
+const numero = $("#numero");
+const boton2 = $("#boton2");
 
 // 2 - funciones
 
 // https://randomuser.me/
-const llamarAPI = e => {
-  e.preventDefault();
+const llamarAPI = urlpersonalizada => {
   $.ajax({
-    url:
-      "https://randomuser.me/api/?nat=es&inc=gender,name,nat,picture&results=5",
+    url: urlpersonalizada,
     dataType: "json",
     success: function(data) {
       console.log(data);
@@ -19,24 +19,43 @@ const llamarAPI = e => {
         data.results[0].name.first,
         data.results[0].name.last
       );
-      // creamos una variable e insertamos el html
-      let miContenido =
-        /* html */
-        `<img class="rounded-circle" src="${data.results[0].picture.large}">
-                <hr>
-                <p>
-                ${data.results[0].name.title}.
-                ${data.results[0].name.first}
-                ${data.results[0].name.last}
-                </p>
-            `;
-      // insertamos el contenido de la variable dentro del div
-      contenido.html(miContenido);
+      let micontenido = "";
+      for (let i = 0; i < data.results.length; i++) {
+        micontenido +=
+          /* html */
+          `<img class="rounded-circle" src="${data.results[i].picture.large}">
+            <hr>
+            <p>
+            ${data.results[i].name.title}.
+            ${data.results[i].name.first}
+            ${data.results[i].name.last}
+            </p>
+        `;
+      }
+      contenido.html(micontenido);
     },
     error: function() {
       console.log("Ha habido un error en la consulta Ajax...");
     }
   });
+};
+
+const PrepararURL = e => {
+  e.preventDefault();
+  console.log(e.target.id);
+  let miurl;
+  if (e.target.id === "boton") {
+    miurl = "https://randomuser.me/api/?nat=es&inc=gender,name,nat,picture";
+  }
+  if (e.target.id === "boton2") {
+    let n = numero.val(); // val es en jquery lo que value en js
+    miurl =
+      "https://randomuser.me/api/?nat=es&inc=gender,name,nat,picture&results=" +
+      n;
+  }
+  console.log(miurl);
+
+  llamarAPI(miurl);
 };
 
 // 3 - eventos
@@ -49,4 +68,5 @@ $('#boton').click(function (e) {
 */
 
 // equivalente a lo de arriba
-boton.on("click", llamarAPI);
+boton.on("click", PrepararURL);
+boton2.on("click", PrepararURL);
